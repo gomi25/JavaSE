add917081c37d358968d68cf7316d9ae3d1e4b1f
User <User@User>
2023-09-22 11:10:38 +0900

    Var

---
+++ src/section07/Card.java
@@ -0,0 +1,14 @@
+package section07;
+
+public class Card {
+	
+	// 기본형, 전역변수, 정적변수(static)
+	public static int width = 62;
+	public static int height = 88;
+	
+	// 참조형, 전역변수, 동적변수
+	public String type = "Spade";
+	public String num = "A";
+	
+
+}
