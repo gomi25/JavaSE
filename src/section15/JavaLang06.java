1cf22cf7262f09e95e9ee530073961d429ff2c5f
User <User@User>
2023-10-20 21:31:52 +0900

    JavaLang

--- src/section15/JavaLang06.java
+++ src/section15/JavaLang06.java
@@ -16,7 +16,7 @@ public class JavaLang06 {
 		int num = Integer.parseInt(strNum);	// 문자열을 int 타입의 값으로 변환하는 메서드
 		System.out.println("num: " + (num+1));
 		
-		Integer num2 = 10;	// 직접 대입도 가능 = 오토박싱 (기본형을 래퍼클래스 타입으로 바꿔주는 것)
+		Integer num2 = 10;	// 직접 대입도 가능 = 오토박싱 (기본형을 래퍼클래스 타입으로 바꿔주는 것). new연산다 없이도 참조형 객체
 		int num3 = Integer.valueOf(10);	// 오토언박싱 (자동으로 기본형으로 바꿔주는 것)
 		// 밑줄로 그인 건 경우가 자바에서 권장하지 않기 때문..오토박싱, 오토언박싱 쓰길 바라는 것. 생성자를 통한 선언은 JDK 1.9부터 사용하지 않는 것을 권장
 		Integer num4 = new Integer(10); // new 써서 하면 주소값이 달라짐
