17878ac2f0eb8cb39256ae5aaaf6a432d9839edf
User <User@User>
2023-10-25 11:24:43 +0900

    Array

--- src/section06/Array04.java
+++ src/section06/Array04.java
@@ -4,7 +4,7 @@ public class Array04 {
 	public static void main(String[] args) {
 		// 2중 배열 선언하기
 		int[][] nums = new int[4][3];
-		
+				
 		nums[0][0] = 1;
 		nums[0][1] = 2;
 		nums[0][2] = 3;
@@ -25,9 +25,5 @@ public class Array04 {
 			}
 			System.out.println();
 		}
-		
-		
-		
 	}
-
 }
